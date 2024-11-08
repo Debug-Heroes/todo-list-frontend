20f4a7ca097256475ef9fe816adb8b84b9073036
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-08 10:58:45 -0300

    chore: configure route sign in

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/home';
+import { SignIn } from '../pages/signIn';
+
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
   },
 ]);
