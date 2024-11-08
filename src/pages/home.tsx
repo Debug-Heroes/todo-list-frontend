0fffa12aa6639c16c754e567b9c47b0dbc983559
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-08 11:18:40 -0300

    refactor: correct import path with alias

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
-import Button from "../components/Button/Button";
+import Button from "@/components/Button/Button";
 
 export function Home() {
   return (
     <>
       <h1 className="text-white">teste</h1>
-      <Button label="Add New Task"/>
+      <Button label="Add New Task" />
     </>
   )
 }
