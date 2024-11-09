de90bc4e8d0d329c8348a3842b322d5e5712397f
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-08 22:27:18 -0300

    feat: card for new tasks

---
+++ src/components/Card/CardNewTask.tsx
@@ -0,0 +1,10 @@
+import { Plus } from "lucide-react"
+
+
+export function CardNewTask() {
+  return (
+    <div className="flex justify-center items-center bg-gray-200 w-full h-[181px]  rounded-md border border-dh-gray-300 shadow py-20">
+      <Plus className="text-gray-400" />
+    </div>
+  )
+}
