a76ae451414d00432a301dd1f23c3cdc2c865972
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-10 12:06:17 -0300

    feat: authentication user with api

--- src/components/Inputs/InputPasswordWithIcon.tsx
+++ src/components/Inputs/InputPasswordWithIcon.tsx
@@ -1,21 +1,21 @@
 import { Eye, EyeClosed } from "lucide-react";
-import { ComponentProps, useState } from "react";
+import { ComponentProps, forwardRef, useState } from "react";
 
 interface InputPasswordWithIconProps extends ComponentProps<'input'> {
   title: string
 }
-export function InputPasswordWithIcon({ title, ...rest }: InputPasswordWithIconProps) {
+export const InputPasswordWithIcon = forwardRef<HTMLInputElement, InputPasswordWithIconProps>(({ title, itemRef, ...rest }, ref) => {
   const [isVisiblePassword, setIsVisiblePassword] = useState(false)
 
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={title}>{title}</label>
       <div className="flex justify-between rounded-sm items-center bg-white gap-2">
-        <input  {...rest} className="flex-1 rounded-sm p-2 bg-transparent" id={title} type={isVisiblePassword ? "text" : "password"} />
+        <input className="flex-1 rounded-sm p-2 bg-transparent" id={title} type={isVisiblePassword ? "text" : "password"} {...rest} ref={ref} />
         {isVisiblePassword && <EyeClosed className="mr-2 cursor-pointer" onClick={() => setIsVisiblePassword(!isVisiblePassword)} />}
         {!isVisiblePassword && <Eye className="mr-2 cursor-pointer" onClick={() => setIsVisiblePassword(!isVisiblePassword)} />}
 
       </div>
     </div>
   )
-}
+})
--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,29 +1,86 @@
 import Button from "@/components/Button/Button";
 import { InputPasswordWithIcon } from "@/components/Inputs/InputPasswordWithIcon";
-import { Link } from "react-router-dom";
+import { signIn } from "@/http/users/sign-in";
+import { api } from "@/libs/axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
 
 
+const UsersSignInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+})
 
+type UsersSignInSchemaForm = z.infer<typeof UsersSignInSchema>
 
 export function SignIn() {
+  const token = sessionStorage.getItem('token')
+
+  const navigate = useNavigate()
+  const { handleSubmit, register, control } = useForm<UsersSignInSchemaForm>({
+    resolver: zodResolver(UsersSignInSchema),
+    defaultValues: {
+      password: '',
+      email: ''
+    }
+  })
+
+  const { mutate } = useMutation({
+    mutationFn: ({ email,
+      password }: UsersSignInSchemaForm) => signIn({
+        email,
+        password
+      }),
+    onSuccess(data, _, __) {
+      api.defaults.headers.common = { 'Authorization': `bearer ${data}` }
+      sessionStorage.setItem('token', data);
+      navigate('/')
+    },
+  })
+
+  function handleSubmitFromLogin(data: UsersSignInSchemaForm) {
+    mutate({
+      ...data
+    })
+  }
+
+  useEffect(() => {
+    if (token) {
+      navigate('/')
+    }
+  }, [token])
   return (
     <div className="w-full h-screen flex  justify-center items-center bg-gray-200 border  text-black p-1">
-      <form className="flex flex-col gap-4 w-full max-w-sm shadow-xl bg-gray-100  rounded-md  p-8">
+      <form onSubmit={handleSubmit(handleSubmitFromLogin)} className="flex flex-col gap-4 w-full max-w-sm shadow-xl bg-gray-100  rounded-md  p-8">
         <h2 className="text-2xl font-bold leading-2 py-4">Log in</h2>
         <div className="flex flex-col gap-2">
           <label htmlFor="email">E-mail</label>
-          <input placeholder="Enter your e-mail" className="rounded-sm p-2" id="email" type="email" />
+          <input {...register('email')} placeholder="Enter your e-mail" className="rounded-sm p-2" id="email" type="email" />
         </div>
-        <InputPasswordWithIcon
-          title="Password"
-          placeholder="Enter your password"
+        <Controller
+          control={control}
+          name="password"
+
+          render={({ field }) => {
+            return (
+              <InputPasswordWithIcon
+                title="Password"
+                placeholder="Enter your password"
+                {...field}
+              />
+            )
+          }}
+
         />
 
         <Link className="text-right text-sm text-red-900" to={'/recover-password'}>
           Forgot password
         </Link>
         <Button
-
           type="submit"
           label={'Log In'}
         />
