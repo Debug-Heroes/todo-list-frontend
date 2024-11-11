df3144f356b796d6d1e466e42da508c7fa14a61a
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-11 20:43:43 -0300

    feat: add authentication layout to sign-in route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/home';
 import { SignIn } from '../pages/signIn';
 import { AppLayout } from '@/pages/_layouts/app';
+import { AuthLayout } from '@/pages/_layouts/auth';
 
 
 export const router = createBrowserRouter([
@@ -9,11 +10,15 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     children: [{
-      path: '/', element: <Home />
+      path: '/', element: <Home />,
     }]
   },
   {
-    path: '/sign-in',
-    element: <SignIn />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [{
+      path: '/sign-in', element: <SignIn />
+    }]
+
   },
 ]);
