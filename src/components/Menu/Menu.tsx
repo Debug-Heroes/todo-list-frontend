787c51c415518dc28a566ebf399dccbb61c46b6f
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-11 20:39:44 -0300

    feat: add logout option to button menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,9 @@
 import { Bolt, Calendar, Clock, Folder, House, LogOut } from "lucide-react";
 import imgBug from '@/assets/debug.svg'
 import { LinkMenu } from "./LinkMenu";
+import { useNavigate } from "react-router-dom";
 export function Menu() {
+  const navigate = useNavigate()
   return (
     <header className="w-[100px] h-full flex flex-col justify-between py-4 items-center bg-white">
       <div>
@@ -30,9 +32,12 @@ export function Menu() {
         />
 
       </div>
-      <div>
+      <button onClick={() => {
+        sessionStorage.clear()
+        navigate('/sign-in')
+      }}>
         <LogOut className="text-dh-gray-300 hover:text-dh-gray-300/60" />
-      </div>
+      </button>
     </header>
   )
 }
