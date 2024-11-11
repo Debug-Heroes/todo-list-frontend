a6169883fd8c251eba95ef9fb593bf8b0a5ea998
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-11 20:37:39 -0300

    feat: add API request for authentication route

--- src/infra/http/users/sign-in.ts
+++ src/infra/http/users/sign-in.ts
@@ -8,13 +8,11 @@ interface SignInRequest {
 
 
 export async function signIn({email, password }: SignInRequest){
-  try{ 
+  
     const tokenUser = await api.post<string>('/login', {
       email, password
     })
   
     return tokenUser.data
-  }catch(err){
-    throw new Error()
-  }
+
 }
