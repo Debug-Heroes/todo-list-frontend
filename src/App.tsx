46475ccd57c3820b2ac746dc5c2391f90ed94c23
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-11 20:36:01 -0300

    chore: add and configure lib sonner for toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { QueryClientProvider } from "react-query";
 import { Router } from "./routes/router-provider";
 import { queryClient } from "./libs/react-query";
+import { Toaster } from "sonner";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster
+        position="top-right"
+        richColors
+      />
       <Router />
     </QueryClientProvider>
   )
