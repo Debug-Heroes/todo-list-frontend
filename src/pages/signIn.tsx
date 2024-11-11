ad2a406a5b8648e498fbf7838769c84385360b72
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-11 20:47:39 -0300

    feat: add error message in mutate, text for sign-up, and loading state in button

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,24 +1,26 @@
 import Button from "@/components/Button/Button";
 import { InputPasswordWithIcon } from "@/components/Inputs/InputPasswordWithIcon";
-import { signIn } from "@/http/users/sign-in";
+import { Loading } from "@/components/Loading/Loading";
+import { signIn } from "@/infra/http/users/sign-in";
 import { api } from "@/libs/axios";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import { AxiosError } from "axios";
+
 import { Controller, useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 import { z } from "zod";
 
 
 const UsersSignInSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(4),
+  email: z.string().email().trim(),
+  password: z.string().min(4).trim(),
 })
 
 type UsersSignInSchemaForm = z.infer<typeof UsersSignInSchema>
 
 export function SignIn() {
-  const token = sessionStorage.getItem('token')
 
   const navigate = useNavigate()
   const { handleSubmit, register, control } = useForm<UsersSignInSchemaForm>({
@@ -29,17 +31,28 @@ export function SignIn() {
     }
   })
 
-  const { mutate } = useMutation({
+  const { mutate, isLoading } = useMutation<string, AxiosError, UsersSignInSchemaForm>({
     mutationFn: ({ email,
-      password }: UsersSignInSchemaForm) => signIn({
+      password }: UsersSignInSchemaForm) => {
+      return signIn({
         email,
         password
-      }),
+      })
+    },
     onSuccess(data, _, __) {
       api.defaults.headers.common = { 'Authorization': `bearer ${data}` }
       sessionStorage.setItem('token', data);
       navigate('/')
     },
+    onError(error: AxiosError, variables, context) {
+      if (error.response?.status === 401) {
+        toast.error('Email or password incorrect')
+      } else {
+        toast.error("Unknown error occurred")
+      }
+
+
+    }
   })
 
   function handleSubmitFromLogin(data: UsersSignInSchemaForm) {
@@ -48,14 +61,10 @@ export function SignIn() {
     })
   }
 
-  useEffect(() => {
-    if (token) {
-      navigate('/')
-    }
-  }, [token])
+
   return (
     <div className="w-full h-screen flex  justify-center items-center bg-gray-200 border  text-black p-1">
-      <form onSubmit={handleSubmit(handleSubmitFromLogin)} className="flex flex-col gap-4 w-full max-w-sm shadow-xl bg-gray-100  rounded-md  p-8">
+      <form onSubmit={handleSubmit(handleSubmitFromLogin)} className="flex flex-col gap-4 w-full max-w-md shadow-xl bg-gray-100  rounded-md  p-8">
         <h2 className="text-2xl font-bold leading-2 py-4">Log in</h2>
         <div className="flex flex-col gap-2">
           <label htmlFor="email">E-mail</label>
@@ -76,13 +85,24 @@ export function SignIn() {
           }}
 
         />
+        <div className="flex justify-between items-center text-xs gap-2">
+          <div className="flex gap-1 items-center">
+            <p>
+              Don't have an account?
+            </p>
+            <Link className="relative after:w-0 after:hover:w-full after:transition-all ease-in-out duration-150 after:h-[1px] after:bg-blue-500 after:absolute after:-bottom-1 after:left-0" to={'/sign-up'}>
+              Sign up here.
+            </Link>
+          </div>
+          <Link className="text-right text-sm text-red-900" to={'/recover-password'}>
+            Forgot password
+          </Link>
+        </div>
 
-        <Link className="text-right text-sm text-red-900" to={'/recover-password'}>
-          Forgot password
-        </Link>
         <Button
+          disabled={isLoading}
           type="submit"
-          label={'Log In'}
+          label={isLoading ? <Loading /> : 'Log In'}
         />
       </form>
     </div>
