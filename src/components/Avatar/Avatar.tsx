5abc6216f38dc7ec19a30308515a55ff7d33ec66
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-16 20:29:31 -0300

    refactor: create avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,7 @@
+export function Avatar() {
+  return (
+    <div className="size-7 rounded-full bg-red-300 shadow-md">
+      <img className="w-full h-full rounded-full" src={'https://avatars.githubusercontent.com/u/67187166'} alt="" />
+    </div>
+  )
+}
